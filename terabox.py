1cd5a479f4118ef6eebf5e5d3245399bced3921c
TmrBotz <tmrbotz@gmail.com>
2024-10-27 21:45:23 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -56,7 +56,7 @@ async def start_command(client, message):
     join_button = InlineKeyboardButton("Tᴍʀ ʙᴏᴛᴢ", url="https://t.me/TMR_Botz")
     developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/TMR_DEVELOPER")
     reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
-    video_file_id = "/app/Jet-Mirror.mp4"
+    video_file_id = "/app/VID_20240424_202515_909.mp4"
     if os.path.exists(video_file_id):
         await client.send_video(
             chat_id=message.chat.id,
